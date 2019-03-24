a8f812363f0af0984595e550456a894537260007
Andee13 <39676479+Andee13@users.noreply.github.com>
2019-03-24 20:24:11 +0200

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ComsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0; // лічильник слів, де d стоїть на 2-му місці
+            string line; // допоміжний рядок
+            char[] separator = {' '}; // масив
+            StringBuilder otvet = new StringBuilder(); // рядок-відповідь
+
+            using (StreamReader MyFile = new StreamReader("text.txt"))
+            {
+                StringBuilder t = new StringBuilder();
+             
+                while ((line = MyFile.ReadLine()) != null )
+                {
+                    //Console.WriteLine(line); // друкуємо, що прочитали
+                    //виділяємо слова Москаленко А.С.
+                    string[] words = line.Split(separator);
+                    
+                    //Console.WriteLine(words.Length);
+                    foreach(string word in words)
+                    {
+                        //Console.WriteLine(word.Length);
+                        t.Append(word[word.Length - 1]);
+                    }
+                }
+                
+                Console.WriteLine(t);
+                
+            }
+
+           
+            
+        }
+    }
+}
